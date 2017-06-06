e259dced0faaf027f3318e10c7fb4629fa32ea3d
Harold Rodriguez <Harold.rodriguez@aztec.com.au>
2017-06-06 15:03:44 +1000

    Added product list component and boostrap package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{pageTitle}}</h1>
+    <pm-products></pm-products>
+  
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent {
+  pageTitle: string = 'Acme product management';
+}
---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: 'app/products/product-list.html'
+})
+
+export class ProductListComponent { } 
